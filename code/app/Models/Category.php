f61fa7841a308ef89a94d6195d8fcf06e17e56d7
❄Kiyonori SAKAI❄ <neco.kiyonori+3@gmai.com>
2021-11-19 17:36:55 +0900

    Models/Category モデル

---
+++ code/app/Models/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property int    $id
+ * @property string $name      カテゴリ名
+ * @property bool   $suspended 一時停止中のカテゴリか否か
+ * @property int    $sequence  並び順
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property Carbon $deleted_at
+ */
+class Category extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'suspended',
+        'sequence',
+    ];
+
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
