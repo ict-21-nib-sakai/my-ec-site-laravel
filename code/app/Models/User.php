fccb7cbf3071598f806165919480ec8d487bc529
❄Kiyonori SAKAI❄ <neco.kiyonori+3@gmai.com>
2021-11-22 11:09:59 +0900

    Models/User - カラムとリレーションシップを追加。

--- code/app/Models/User.php
+++ code/app/Models/User.php
@@ -2,15 +2,34 @@
 
 namespace App\Models;
 
+use Carbon\Traits\Timestamp;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property string        $id
+ * @property string        $name
+ * @property string        $email
+ * @property Carbon | null $email_verified_at
+ * @property string        $password
+ * @property string | null $remember_token
+ * @property string        $home_address
+ * @property bool          $suspended
+ * @property Carbon | null $created_at
+ * @property Carbon | null $updated_at
+ * @property Carbon | null $deleted_at
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use Timestamp;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +40,15 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'remember_token',
+        'home_address',
+        'suspended',
+    ];
+
+    protected $guarded = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
     ];
 
     /**
@@ -41,4 +69,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    public function cart(): HasMany
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function order(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
