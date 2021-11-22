d9af6adc142f5c57cffa374c34742d437752671c
❄Kiyonori SAKAI❄ <neco.kiyonori+3@gmai.com>
2021-11-22 11:11:11 +0900

    factories/UserFactory - カラムを追加したので合わせて調整した。

--- code/database/factories/UserFactory.php
+++ code/database/factories/UserFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use JetBrains\PhpStorm\ArrayShape;
 
 class UserFactory extends Factory
 {
@@ -12,23 +14,38 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    #[ArrayShape([
+        'name'              => "string",
+        'email'             => "string",
+        'email_verified_at' => Carbon::class,
+        'password'          => "string",
+        'remember_token'    => "string",
+        'home_address'      => "string",
+        'suspended'         => "bool",
+        'created_at'        => Carbon::class,
+        'updated_at'        => Carbon::class,
+    ])]
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name'              => $this->faker->name(),
+            'email'             => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+            'home_address'      => $this->faker->address(),
+            'suspended'         => $this->faker->boolean(10),
+            'created_at'        => now(),
+            'updated_at'        => now(),
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return Factory
      */
-    public function unverified()
+    public function unverified(): Factory
     {
         return $this->state(function (array $attributes) {
             return [
