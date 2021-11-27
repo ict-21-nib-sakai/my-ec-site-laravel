6802ccff31ed49209f4fbbd60c3d456eb8285ee6
❄Kiyonori SAKAI❄ <ict21sakai@outlook.jp>
2021-11-27 10:05:30 +0900

    $ php artisan ui vue --auth

--- code/routes/web.php
+++ code/routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [Controllers\IndexController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
