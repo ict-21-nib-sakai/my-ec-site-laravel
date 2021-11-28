cee83a23c0e8ca28bb9b8e90d52884df0ebdee47
❄Kiyonori SAKAI❄ <ict21sakai@outlook.jp>
2021-11-28 20:32:02 +0900

    routes/web.php - ルート設定

--- code/routes/web.php
+++ code/routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [Controllers\IndexController::class, 'index']);
+Route::get('/', [Controllers\IndexController::class, 'index'])
+    ->name('index');
 
 Auth::routes();
 
